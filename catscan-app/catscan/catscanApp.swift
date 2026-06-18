1b2b2109d9c8c53a3546f0e8dc12031601e90212
Marvin Tseng <marvin@tseng.ch>
2026-06-18 20:50:29 +0200

    init app

---
+++ catscan-app/catscan/catscanApp.swift
@@ -0,0 +1,17 @@
+//
+//  catscanApp.swift
+//  catscan
+//
+//  Created by Marvin Tseng on 18.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct catscanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ catscan-app/catscanTests/catscanTests.swift
@@ -0,0 +1,19 @@
+//
+//  catscanTests.swift
+//  catscanTests
+//
+//  Created by Marvin Tseng on 18.06.2026.
+//
+
+import Testing
+@testable import catscan
+
+struct catscanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
